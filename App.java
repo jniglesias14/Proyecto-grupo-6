ab8c9b191563217d1da418ae0c6365aab5b9457b
jniglesias14 <159542218+jniglesias14@users.noreply.github.com>
2024-03-08 17:56:25 +0100

    Update App.java

--- App.java
+++ App.java
@@ -1,3 +1,4 @@
+import javax.sound.midi.Soundbank;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Iterator;
@@ -8,18 +9,17 @@ public class App {
         String opcion;
         int n;
         boolean seguir = true;
-/*
+
+
         while(seguir){
-            System.out.println("dime la opcion \n1)menu administrador \n2)menu departamento");
+            System.out.println("dime la opcion \n1)menu administrador \n2)menu departamento \n3)salir");
             opcion=in.nextLine();
             n=Integer.parseInt(opcion);
             if(n==1){
                 System.out.println("dime la contraseña");
                 contraseña=in.nextLine();
                 if(contraseña.equals(Administrador.password)){
-                    System.out.println("dime la opcion");
-                    opcion= in.nextLine();
-                    Administrador.MenuAdmin(opcion);
+                    Administrador.MenuAdmin();
                 }
                 else{
                     System.out.println("contraseña erronea");
@@ -27,38 +27,44 @@ public class App {
             }else if(n==2){
                 System.out.println("dime la clave");
                 opcion=in.nextLine();
-                Administrador a=new Administrador();
-                if(a.comprobar(opcion)){
-                    System.out.println("dime la clave");
-                    opcion=in.nextLine();
-                    Departamento.MenuDepartamento(opcion);
-                }
-                else{
-                    System.out.println("no existe el codigo departamento");
+                if(Administrador.compararDepartamento2(opcion)){
+                    Departamento.MenuDepartamento();
+                }else{
+                    System.out.println("no existe ese departamento");
                 }
 
 
-
-            }else{
+            }else if(n==3){
+                System.out.println("adios");
                 seguir=false;
+            }
+            else{
+                System.out.println("error");
             }
         }
 
-*/
 
 
-        Administrador a1=new Administrador();
+
+/*
         Reserva r1 = new Reserva(2024, 2, 2, 9, 2);
         Reserva r2 = new Reserva(2024, 2, 3, 9, 2);
         Reserva r3 = new Reserva(2024, 2, 4, 9, 2);
-        a1.añadirSalas("juntas","J");
-        a1.añadirSalas("cine","C");
-        a1.añadirSalas("biblioteca","B");
-        a1.añadirReservaSalas(r1,"J");
-        a1.añadirReservaSalas(r2,"C");
-        a1.añadirReservaSalas(r3,"B");
-        a1.cancelarReservaSalas(r1,"J");
-        a1.ListarSalas();
+        Administrador.añadirSalas("juntas","J");
+        Administrador.añadirSalas("cine","C");
+        Administrador.añadirSalas("biblioteca","B");
+        Administrador.añadirReservaSalas(r1,"J");
+        Administrador.añadirReservaSalas(r2,"C");
+        Administrador.añadirReservaSalas(r3,"B");
+        Administrador.ListarSalas();
+        System.out.println("----");
+        Administrador.cancelarReservaSalas(r1,"J");
+        Administrador.ListarSalas();
+        System.out.println("-----");
+        Administrador.cancelarReservaSalas(r2,"C");
+        Administrador.ListarSalas();
+
+*/
 
     }
 }
