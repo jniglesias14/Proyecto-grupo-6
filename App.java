81600d227e061228ae37433431e1a0f96b8f107d
Dianshiro <110417963+Dianshiro@users.noreply.github.com>
2024-03-09 15:28:53 +0100

    Mejora menu principal

--- App.java
+++ App.java
@@ -5,66 +5,54 @@ import java.util.Iterator;
 public class App {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String contraseña;
-        String opcion;
-        int n;
-        boolean seguir = true;
+        int n = -1;
 
-
-        while(seguir){
-            System.out.println("dime la opcion \n1)menu administrador \n2)menu departamento \n3)salir");
-            opcion=in.nextLine();
-            n=Integer.parseInt(opcion);
-            if(n==1){
-                System.out.println("dime la contraseña");
-                contraseña=in.nextLine();
-                if(contraseña.equals(Administrador.password)){
-                    Administrador.MenuAdmin();
-                }
-                else{
-                    System.out.println("contraseña erronea");
-                }
-            }else if(n==2){
-                System.out.println("dime la clave");
-                opcion=in.nextLine();
-                if(Administrador.compararDepartamento2(opcion)){
-                    Departamento.MenuDepartamento();
-                }else{
-                    System.out.println("no existe ese departamento");
-                }
-
-
-            }else if(n==3){
-                System.out.println("adios");
-                seguir=false;
+        // Mientras no se elija la opcion 3
+        while(n != 3) {
+            System.out.println("Bienvenido");
+            System.out.println("1) Login Administrador");
+            System.out.println("2) Login Departamento");
+            System.out.println("3) Salir");
+            // Seleccion de opcion con control de errores
+            try {
+                n = Integer.parseInt(in.nextLine());
+            } catch (NumberFormatException e) {
+                n = -1;
             }
-            else{
-                System.out.println("error");
+            switch(n) {
+                case 1: {
+                    // Login Administrador
+                    System.out.println("Introduzca la contraseña");
+                    String pass = in.nextLine();
+                    if(pass.equals(Administrador.password)){
+                        Administrador.MenuAdmin();
+                    }else{
+                        System.out.println("contraseña erronea");
+                    }
+                    break;
+                }
+                case 2: {
+                    // Login Departamento
+                    System.out.println("Introduzca la clave");
+                    String clave = in.nextLine();
+                    if(Administrador.compararDepartamento2(clave)){
+                        Departamento.MenuDepartamento();
+                    }else{
+                        System.out.println("no existe ese departamento");
+                    }
+                    break;
+                }
+                case 3: {
+                    // Salir sistema
+                    System.out.println("Adios");
+                    break;
+                }
+                default: {
+                    // Opcion erronea
+                    System.out.println("Opcion erronea");
+                    break;
+                }
             }
         }
-
-
-
-
-/*
-        Reserva r1 = new Reserva(2024, 2, 2, 9, 2);
-        Reserva r2 = new Reserva(2024, 2, 3, 9, 2);
-        Reserva r3 = new Reserva(2024, 2, 4, 9, 2);
-        Administrador.añadirSalas("juntas","J");
-        Administrador.añadirSalas("cine","C");
-        Administrador.añadirSalas("biblioteca","B");
-        Administrador.añadirReservaSalas(r1,"J");
-        Administrador.añadirReservaSalas(r2,"C");
-        Administrador.añadirReservaSalas(r3,"B");
-        Administrador.ListarSalas();
-        System.out.println("----");
-        Administrador.cancelarReservaSalas(r1,"J");
-        Administrador.ListarSalas();
-        System.out.println("-----");
-        Administrador.cancelarReservaSalas(r2,"C");
-        Administrador.ListarSalas();
-
-*/
-
     }
 }
